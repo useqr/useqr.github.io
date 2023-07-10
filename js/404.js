4a0316591c7ee35eee2c98eefffadddbd0712043
useqr <116953851+useqr@users.noreply.github.com>
2023-07-10 23:58:10 +0700

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,3 +1,4 @@
 var uriD = window.location.href, uriN = uriD.replace(/.+#/,"");
-if (uriD != uriN){window.location="../create/#"+uriN;}
+var path = Math.floor(Math.random() * 90 + 10);
+if (uriD != uriN){window.location="../create/00"+path+"#"+uriN;}
 else {document.title="Page has been deleted"}
