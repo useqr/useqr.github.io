21078fa728fd7c5efc91ad1c2842530beadeeb4c
useqr <116953851+useqr@users.noreply.github.com>
2023-07-29 22:05:59 +0700

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -2,6 +2,9 @@ var uriD = window.location.href, uriN = uriD.replace(/.+#/,"");
 var path = Math.floor(Math.random() * 90 + 10);
 if (uriD != uriN){window.location="../create/00"+path+"#"+uriN;}
 else {
-  if (uriD.includes('link/t/-')  == true){window.location=uriD.replace('link/t/-','link/t/?co=');}
-  else{ document.title="Page has been deleted";}
+  if (uriD.includes('link/t/-')  == true){window.location=uriD.replace('link/t/-','link/t/?co=');}else{ 
+    if (uriD.includes('link/-')  == true){window.location=uriD.replace('link/-','link/t/?co=');}else{ 
+    document.title="Page has been deleted";
+    }
+  }
 }
