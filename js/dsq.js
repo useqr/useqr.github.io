50fb31f139832a7b554dbb60aae7864f33cf54b7
useqr <116953851+useqr@users.noreply.github.com>
2023-11-02 09:25:20 +0700

    Create dsq.js

---
+++ js/dsq.js
@@ -0,0 +1,35 @@
+var TEST_NUMERIC = /^\d+$/, TEST_ALPHANUMERIC = /^[0-9A-Z$%*+-./: ]+$/;
+function chooseBestModeData(data) {
+   if (TEST_NUMERIC.test(data)) { return new QRCode.QRNumeric(data); } else if (TEST_ALPHANUMERIC.test(data)) { return new QRCode.QRAlphanumeric(data); }
+   try { return new QRCode.QRKanji(data); } catch (error) { return new QRCode.QRByte(data); }
+}
+	    
+	var urlasli = window.location.href.replace('?fbclid=','@').replace(/@.+/,'');
+	var kosong = urlasli.replace(/sq?=.+/,"sq?="); if (urlasli == kosong) {window.location="/404"}
+	var takde = kosong.replace('sq?=',''); if (urlasli == takde) {window.location="/404"}
+	var akh = urlasli.slice(-1); if (akh == "-") {window.location="/404"}
+	var dataUrl = urlasli.replace(/.+sq?=/,"").split("-");
+	console.log(dataUrl);
+	var posts = '';   
+	for (var i = 0; i < dataUrl.length; i++) {   
+	var tcoUrl = 'http://disq.us/t/'+dataUrl[i];
+
+ var data = tcoUrl;
+ var mode = 'Auto'; var ecLevel = 'L'; var margin = '5'; var moduleSize = '8'; var hasEncodingHint = false;
+ var qrcode = new QRCode.Encoder();
+ var errorCorrectionLevel = QRCode.ErrorCorrectionLevel[ecLevel];
+ qrcode.setEncodingHint(hasEncodingHint).setErrorCorrectionLevel(errorCorrectionLevel);
+ try {
+	var data = mode === 'Auto' ? chooseBestModeData(data) : new QRCode[mode](data);
+	qrcode.write(data).make();
+	var srcimgQR = qrcode.toDataURL(moduleSize, margin); //console.log(srcimgQR);
+ } catch (error) { alert(error.message); }	  
+ var imgQR = '<img id="srcQR" class="srcQR" src="'+srcimgQR+'"></img>';
+	
+	var hasil  = '<tr><td>'+imgQR+'</td><td>Part '+(i+1)+' <a href="'+tcoUrl+'">'+tcoUrl+'</a></td></tr>';	
+	posts += hasil;	
+	}
+	var postresult = '<table class="tableresult">'+posts+'</table>';
+	if (urlasli != takde) {
+	document.getElementById('post').innerHTML = postresult;
+	} else {window.location="/404";}
