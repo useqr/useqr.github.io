7cbd23363eabffaef097c8f331d35082fb781d23
useqr <116953851+useqr@users.noreply.github.com>
2024-06-07 02:38:42 +0700

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,5 +1,5 @@
 var uriD = window.location.href, uriN = uriD.replace(/.+#/,"");
-var path = Math.floor(Math.random() * 90 + 10), onepath = Math.floor(Math.random() * 9);
+var path = Math.floor(Math.random() * 90 + 10), onepath = Math.floor(Math.random() * 9), fiveDigitNumber = Math.floor(Math.random() * 90000) + 10000;
 if (uriD != uriN){window.location="../create/00"+path+"#"+uriN;}
 else {
   if (uriD.includes('link/t/-')  == true){window.location=uriD.replace('link/t/-','link/t/'+onepath+'?co=');}else{ 
@@ -9,7 +9,7 @@ else {
           if (uriD.includes('link/bit-')  == true){window.location=uriD.replace('link/bit-','link/b/'+onepath+'?ly=');}else{
             if (uriD.includes('link/v-')  == true){
               var code = uriD.replace(/.+v-/,"").replace("&m=1","").replace('?fbclid=','@').replace('&fbclid=','@').replace(/@.+/,'');
-              window.location='//draft.useqr.link/'+onepath+onepath+onepath+onepath+onepath+'?id='+Base64.decode(code);}else{
+              window.location='//draft.useqr.link/'+fiveDigitNumber+'?id='+Base64.decode(code);}else{
                 if (uriD.includes('link/prm-')  == true){window.location=uriD.replace('link/prm-','link/prm/'+onepath+'?id=');}else{
                   document.title="Page has been deleted";
                 }
