af04a248d3d4172af7bb74513f57a33cbfc5a95a
useqr <116953851+useqr@users.noreply.github.com>
2024-06-28 23:51:13 +0700

    Update index.js

--- extract/f/index.js
+++ extract/f/index.js
@@ -1,6 +1,6 @@
 const IdCode = window.location.href.replace(/ht.+de=/,"").replace("#","").replace(/\&fbclid.+/,"").toUpperCase();  
 const DoHost = window.location.protocol+'//'+window.location.hostname+window.location.pathname;
-const apiUrl = `https://cdn.jsdelivr.net/gh/useqr/d@master/${IdCode}.json`, resultContainer = document.getElementById("nobat");
+const apiUrl = `//raw.githack.com/useqr/d/main/${IdCode}.json`, resultContainer = document.getElementById("nobat");
 
 function ngonsole(){              
 	  $(document).ready(function(){                      				
